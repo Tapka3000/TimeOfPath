2c3fd284127e9747fc311407314f1ec1d364f928
Tapka3000 <circle.table@yandex.ru>
2023-10-28 19:37:22 +0300

    commit

--- src/main/java/org/example/Wya_home.java
+++ src/main/java/org/example/Wya_home.java
@@ -1,2 +1,14 @@
-package org.example;public class Wya_home {
+package org.example;
+
+public class Wya_home {
+    public static void main(String[] args) {
+        int path = 5;
+        double speedTaxi = 60;
+        double timeOfPath = (double) path / speedTaxi;
+        double delay = 0.35;
+        double timePathOfHome = timeOfPath + delay;
+
+        System.out.println("Время на путь до дома составляет " + timePathOfHome + " часов");
+
+    }
 }
