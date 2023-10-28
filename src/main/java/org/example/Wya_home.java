fd7cfb95a7d955c90a468215508b05ed739d9449
Tapka3000 <circle.table@yandex.ru>
2023-10-28 19:44:11 +0300

    commit

--- src/main/java/org/example/Wya_home.java
+++ src/main/java/org/example/Wya_home.java
@@ -8,6 +8,10 @@ public class Wya_home {
         double delay = 0.35;
         double timePathOfHome = timeOfPath + delay;
 
+        System.out.println("Расстояние = " + path + " км");
+        System.out.println("Скорость такси = " + speedTaxi + " км/ч");
+        System.out.println("Время в пути = " + timePathOfHome + " часа");
+        System.out.println("Коэфициент замедления = " + delay);
         System.out.println("Время на путь до дома составляет " + timePathOfHome + " часов");
 
     }
